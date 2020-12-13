f1a0d1a73882e1429bb15567b1ad88feba9ab4df
Thompson Edolo <verygreenboi@live.com>
2020-12-13 22:52:16 +0100

    fix: trailing slash bug

--- src/utils/validate.user.middleware.ts
+++ src/utils/validate.user.middleware.ts
@@ -1,38 +1,42 @@
-import * as express from 'express';
-import * as axios from 'axios';
+import * as express from "express";
+import * as axios from "axios";
 
-export const validateUser = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const auth = req.headers.authorization?.split(' ');
-    let token: string;
-    if (!auth || auth.length < 2 || !auth[1]) {
-        res.status(401).send({
-            error: new Error('User is not authenticated')
-        });
-        return;
-    } else {
-        token = auth[1];
-    }
+export const validateUser = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const auth = req.headers.authorization?.split(" ");
+  let token: string;
+  if (!auth || auth.length < 2 || !auth[1]) {
+    res.status(401).send({
+      error: new Error("User is not authenticated"),
+    });
+    return;
+  } else {
+    token = auth[1];
+  }
 
-    const { APP_ID, SERVER_URL } = process.env;
+  const { APP_ID, SERVER_URL } = process.env;
 
-    const config: axios.AxiosRequestConfig = {
-        headers: {
-            'X-Parse-Application-Id': APP_ID,
-            'Content-Type': 'application/json',
-            'X-Parse-Session-Token': token
-        }
-    }
+  const config: axios.AxiosRequestConfig = {
+    headers: {
+      "X-Parse-Application-Id": APP_ID,
+      "Content-Type": "application/json",
+      "X-Parse-Session-Token": token,
+    },
+  };
 
-    const url = SERVER_URL || 'http://localhost:1337/api/';
+  let url = SERVER_URL || "http://localhost:1337/api/";
+  url = url.endsWith("/") ? url.slice(0, -1) : url;
 
-    try {
-        await axios.default.get(`${url}users/me`, config);
-        next();
-    } catch (err) {
-        res.status(403).send({
-            error: new Error(err.message)
-        });
-        return;
-    }
-
-}
+  try {
+    await axios.default.get(`${url}/users/me`, config);
+    next();
+  } catch (err) {
+    res.status(403).send({
+      error: new Error(err.message),
+    });
+    return;
+  }
+};
